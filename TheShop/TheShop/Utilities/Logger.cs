a57f005cacf43b33b7f693ad90404e453326b9b9
MilosVulikic <milosvulikic@gmail.com>
2022-01-14 13:01:42 +0100

    Moving classes out from ShopService.cs to separate files.

---
+++ TheShop/TheShop/Utilities/Logger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TheShop.Utilities
+{
+	public class Logger
+	{
+		// To be replaced with normal log tools if allowed
+		public void Info(string message)
+		{
+			Console.WriteLine("Info: " + message);
+		}
+
+		public void Error(string message)
+		{
+			Console.WriteLine("Error: " + message);
+		}
+
+		public void Debug(string message)
+		{
+			Console.WriteLine("Debug: " + message);
+		}
+	}
+
+}
