46a356e0818bc588762f92499d90cadb0f83ce59
MilosVulikic <milosvulikic@gmail.com>
2022-01-19 22:13:00 +0100

    Change BaseRepository to abstract class

--- TheShop/TheShop/DAL/Repositories/BaseRepository.cs
+++ TheShop/TheShop/DAL/Repositories/BaseRepository.cs
@@ -7,17 +7,17 @@ using TheShop.DAL.Models;
 
 namespace TheShop.DAL.Repositories
 {
-	public class BaseRepository<T> where T : Article    // Article to be replaced with more generic type
+	public abstract class BaseRepository<T> where T : Article    // Article to be replaced with more generic type
 	{
 		protected readonly ApplicationDbContext DatabaseContext;
 		protected readonly DbSet<T> _entities;
 
-		public T Get(int id)
+		public virtual T Get(int id)
 		{
 			return _entities.FirstOrDefault(s => s.ID == id);
 		}
 
-		public T Save(T entity)
+		public virtual T Save(T entity)
 		{
 			var result = _entities.Add(entity);
 			DatabaseContext.SaveChanges();
