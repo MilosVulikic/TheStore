c32540c0a2a255aef66cbfdf3000ff905e3af757
MilosVulikic <milosvulikic@gmail.com>
2022-01-27 08:58:52 +0100

    Removed uneccessary usings

--- TheShop/TheShop/Mappers/ArticleMapper.cs
+++ TheShop/TheShop/Mappers/ArticleMapper.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TheShop.DAL.Models;
+﻿using TheShop.DAL.Models;
 using TheShop.DTOs;
 
 namespace TheShop.Mappers
